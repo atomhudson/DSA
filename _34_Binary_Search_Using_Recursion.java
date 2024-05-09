c4c07dc51556cf93756b019d24ffb8d49864ea61
atomhudson <dhruvsaini0904@gmail.com>
2024-05-10 02:40:04 +0530

    new

---
+++ _34_Binary_Search_Using_Recursion.java
@@ -0,0 +1,30 @@
+public class _34_Binary_Search_Using_Recursion {
+//1.    Comparison take constant time: O(1).
+//2.    Dividing into 2 half.
+
+//   Recurrence Relation
+//   [ F(N) = O(1) + F( N / 2); ]
+//   What out Recurrence relation saying?
+// ->Means for the (N) size array,
+//      we are making a comparison in constant time and
+//      searching the target in the array size (N / 2);
+//   O(1): Comparison that we gonna make.
+//   F(N/2): Dividing into 2 half.
+
+
+//   Types of Recurrence Relation.
+//    1. Linear Recurrence Relation: Fibonacci Number
+//         |
+//          -> This is not getting factored , this is getting subtracted and added linearly.
+//    2. Divide and Conquer Recurrence Relation : Binary Search
+//         |
+//          -> Reduced by a factor
+
+
+
+
+
+    public static void main(String[] args) {
+
+    }
+}

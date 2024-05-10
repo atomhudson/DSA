3fe4997334974a251c8a35f35fe1fef854523424
atomhudson <dhruvsaini0904@gmail.com>
2024-05-11 04:45:05 +0530

    he

--- _34_Binary_Search_Using_Recursion.java
+++ _34_Binary_Search_Using_Recursion.java
@@ -20,11 +20,50 @@ public class _34_Binary_Search_Using_Recursion {
 //         |
 //          -> Reduced by a factor
 
-
-
-
-
-    public static void main(String[] args) {
+    static int BinarySearchRecursion(int[] arr, int target,int start, int end){
+//        int start = 0;
+//        int end = arr.length - 1;
+//        static int BinarySearchRecursion(int[] arr, int target){}
+//        We want that start and end get modified to every function call
+//        reduce the search size of the array (half an array).
+//        So we have to put start and end in argument.
+        if(start > end){
+            return -1;
+        }
+//      Body
+        int middle = start + (end - start) / 2;
+//        middle is not specific for every call.
+        if(arr[middle] == target){
+            return middle;
+        }
+        if(arr[middle] < target){
+//      Tip: Make sure  to return the result of a function call of the return type;
+            return BinarySearchRecursion(arr, target, middle+1, end);
+        }else{
+            return BinarySearchRecursion(arr, target, start, middle-1);
+        }
 
     }
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,6};
+        int target = 1;
+        System.out.println(BinarySearchRecursion(arr,target,0, arr.length-1));
+    }
 }
+//        s________m_______e
+//array = |________|_______|  <-- f()
+//                     |
+//                  s___m___e
+//                  |___|___|  <-- f()
+//                    |
+//                   s_m_e
+//                   |_|_|    <-- f()
+//                    |
+//                   |_| (target)
+
+//  As we can see that start and end variable is important to every call.
+//  In Short if there are a few variables that you need to pass in the future function call put it inside argument.
+//  Now, if start and end variable is important for every call than why not middle also important?
+//  Because middle in only beneficial inside body only or can calculate using start and end variables.
+
+

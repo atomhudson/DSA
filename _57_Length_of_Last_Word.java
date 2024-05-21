c4bfb5930eaff06681a26f084c8da927ecd493d1
atomhudson <dhruvsaini0904@gmail.com>
2024-05-22 00:15:01 +0530

    gfjhhjkj

---
+++ _57_Length_of_Last_Word.java
@@ -0,0 +1,12 @@
+public class _57_Length_of_Last_Word {
+
+    public static int lengthOfLastWord(String s) {
+        String[] words = s.split(" ");
+        return words[words.length-1].length();
+    }
+
+    public static void main(String[] args) {
+        String s = "Hello World";
+        System.out.println(lengthOfLastWord(s));
+    }
+}

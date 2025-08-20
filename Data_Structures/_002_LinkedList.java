7dff26f975c6714f96e38bf3f3928356bf726465
atomhudson <dhruvsaini0904@gmail.com>
2025-08-20 21:22:02 +0530

    single linked list

--- Data_Structures/_002_LinkedList.java
+++ Data_Structures/_002_LinkedList.java
@@ -29,6 +29,34 @@ public class _002_LinkedList {
         }
         head = new_node;
     }
+    public void insertInLast(int value){
+        Node new_node = new Node(value);
+        if (head == null){
+            head = new_node;
+            return;
+        }
+        Node temp = head;
+        while (temp.next != null){
+            temp = temp.next;
+        }
+        temp.next = new_node;
+    }
+
+    public void insertInSpecificPosition(int value, int index){
+        Node new_node = new Node(value);
+        if (index == 0 || head == null){
+            new_node.next = head;
+            head = new_node;
+            return;
+        }
+        Node temp = head;
+        for (int i = 0; i < index - 1 && temp.next != null; i++){
+            temp = temp.next;
+        }
+        new_node.next = temp.next;
+        temp.next = new_node;
+    }
+
     public void traverse(){
         Node temp = head;
         while(temp != null){
@@ -36,15 +64,30 @@ public class _002_LinkedList {
             temp = temp.next;
         }
         System.out.print("null");
+        System.out.println();
     }
     public static void main(String[] args) {
         _002_LinkedList linkedList = new _002_LinkedList();
+        linkedList.insertInLast(10);
+        linkedList.insertInLast(20);
+        linkedList.insertInLast(30);
+        linkedList.insertInLast(40);
+        linkedList.insertInLast(50);
+        linkedList.insertInLast(60);
+        linkedList.traverse();
         linkedList.insertInBegining(10);
         linkedList.insertInBegining(20);
         linkedList.insertInBegining(30);
         linkedList.insertInBegining(40);
         linkedList.insertInBegining(50);
         linkedList.insertInBegining(60);
+        linkedList.traverse();
+        linkedList.insertInSpecificPosition(10,0);
+        linkedList.insertInSpecificPosition(20,1);
+        linkedList.insertInSpecificPosition(30,2);
+        linkedList.insertInSpecificPosition(40,3);
+        linkedList.insertInSpecificPosition(50,4);
+        linkedList.insertInSpecificPosition(60,5);
         linkedList.traverse();
     }
 }
